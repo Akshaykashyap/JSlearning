43a0dc55095b27606c136634844f6abe8571cb3a
Akshaykashyap <Akshay95kashyap@gmail.com>
2021-03-04 14:44:09 +0530

    modified app file

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-var country = 'India';
-var continent = 'Asia';
-var population = '1.38 billion';
-console.log(country, ',' ,continent, ','  ,population);
+/* Variable Declaration */
+
+let country = 'India';
+let continent = 'Asia';
+let population = '1.38 billion';
+console.log(country,',',continent,',',population);
+
+
+/* Data Types */
+let isIsland = 1382;
+let  abcd = true;
+let language;
+console.log(isIsland,country,population,language);
